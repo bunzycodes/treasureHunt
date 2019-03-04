f11c23ed22ff8b711ecec5b2c9d11ab0bb1763c0
Jerrod Jordan <jerrodjordan@Tinas-MBP.localdomain>
2019-03-04 10:23:00 -0800

    adds board and square components

---
+++ src/Board.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import Square from './Square'
+
+
+class Board extends Component {
+  render() {
+    return (
+      <div>
+        <Square />
+      </div>
+    );
+  }
+}
+
+export default Board;
