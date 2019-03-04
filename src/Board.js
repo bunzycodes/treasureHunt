30374f17c808f484ad8895028f57fe6e2358c16d
Jerrod Jordan <jerrodjordan@Tinas-MBP.localdomain>
2019-03-04 11:04:09 -0800

    layout

--- src/Board.js
+++ src/Board.js
@@ -1,12 +1,31 @@
 import React, { Component } from 'react';
 import Square from './Square'
+import './App.css'
 
 
 class Board extends Component {
+
+  constructor(props){
+    super(props)
+    this.state = {
+      squares: ['?', '?', '?',
+                '?', '?', '?',
+                '?', '?', '?'],
+    }
+  }
   render() {
+    const { squares } = this.state
     return (
-      <div>
-        <Square />
+      <div className = "board">
+        {squares.map((squareValue, index) => {
+          return (
+            <Square
+            key = {index}
+            value = {squareValue}
+            index = {index}
+            />
+          )
+        })}
       </div>
     );
   }
--- src/Square.js
+++ src/Square.js
@@ -4,8 +4,11 @@ import './App.css'
 
 class Square extends Component {
   render() {
+    const { value } = this.props
+
     return (
       <div className = "square">
+        {value}
       </div>
     );
   }
